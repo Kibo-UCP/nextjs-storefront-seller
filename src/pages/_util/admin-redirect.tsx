17915779e3905b90e8f9f8c9dcbc2c8b72374662
kibo-sushant <sushant.jadhav@kibocommerce.com>
2024-01-09 15:20:46 +0530

    feat: commented code

--- src/pages/_util/admin-redirect.tsx
+++ src/pages/_util/admin-redirect.tsx
@@ -1,37 +1,11 @@
 import React from 'react'
 
-import { parse } from 'url'
-
-import saveSellerToken from '@/lib/api/operations/save-seller-token'
-
-// export async function getServerSideProps(context: any) {
-//   // await saveSellerToken(context.req, context.res)
-
-//   // const { query } = parse(context.req.url as string, true)
-//   // const { redirect, isSeller } = query
-
-//   // Remove "_util" from the current URL and append the redirect path
-//   // const currentPath = context.req.url
-//   // const destination = currentPath.replace(/\/_util\/.*/, `/${redirect}`)
-
-//   // console.log('context.req: ', context.req)
-//   // console.log('currentPath: ', currentPath)
-//   // console.log('destination: ', destination)
-
-//   return {
-//     // redirect: {
-//     //   destination: destination + (isSeller ? '&isSeller=' + isSeller : ''),
-//     //   permanent: false,
-//     // },
-//   }
-// }
-
-const AdminRedirect = () => {
+const index = () => {
   return (
     <div>
-      <h1>Redirecting...</h1>
+      <h1>Test Page</h1>
     </div>
   )
 }
 
-export default AdminRedirect
+export default index
