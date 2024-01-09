36aff0208dd1f8ac73c0ed3c84699fec1bd84382
kibo-sushant <sushant.jadhav@kibocommerce.com>
2024-01-09 16:44:30 +0530

    4

--- src/pages/_util/admin-redirect.tsx
+++ src/pages/_util/admin-redirect.tsx
@@ -4,32 +4,37 @@ import { parse } from 'url'
 
 import saveSellerToken from '@/lib/api/operations/save-seller-token'
 
-// export async function getServerSideProps(context: any) {
-//   await saveSellerToken(context.req, context.res)
+export async function getServerSideProps(context: any) {
+  // await saveSellerToken(context.req, context.res)
 
-//   const { query } = parse(context.req.url as string, true)
-//   const { redirect, isSeller } = query
+  const { query } = parse(context.req.url as string, true)
+  const { redirect, isSeller } = query
 
-//   // Remove "_util" from the current URL and append the redirect path
-//   const currentPath = context.req.url
-//   const destination = currentPath.replace(/\/_util\/.*/, `/${redirect}`)
+  // Remove "_util" from the current URL and append the redirect path
+  const currentPath = context.req.url
+  const destination = currentPath.replace(/\/_util\/.*/, `/${redirect}`)
 
-//   console.log('context.req: ', context.req)
-//   console.log('currentPath: ', currentPath)
-//   console.log('destination: ', destination)
+  console.log('context.req: ', context.req)
+  console.log('currentPath: ', currentPath)
+  console.log('destination: ', destination)
 
-//   return {
-//     redirect: {
-//       destination: destination + (isSeller ? '&isSeller=' + isSeller : ''),
-//       permanent: false,
-//     },
-//   }
-// }
+  return {
+    // redirect: {
+    //   destination: destination + (isSeller ? '&isSeller=' + isSeller : ''),
+    //   permanent: false,
+    // },
+    props: {   
+      currentPath: currentPath,
+      url: destination + (isSeller ? '&isSeller=' + isSeller : ''),
+    },
+  }
+}
 
-const AdminRedirect = () => {
+const AdminRedirect = (props: {  currentPath: any; url: string }) => {
   return (
-    <div>
-      <h1>_util Redirecting...</h1>
+    <div>    
+      <h2>currentPath: {props.currentPath}</h2>
+      <h2>url: {props.url}</h2>
     </div>
   )
 }
