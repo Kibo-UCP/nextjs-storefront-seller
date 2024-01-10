5300d49a95584a4eecdc7c26e1f34cde39ad36d6
kibo-sushant <sushant.jadhav@kibocommerce.com>
2024-01-10 15:06:57 +0530

    feat: added console

--- src/pages/_util/admin-redirect.tsx
+++ src/pages/_util/admin-redirect.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 import { parse } from 'url'
 
 import saveSellerToken from '@/lib/api/operations/save-seller-token'
+import { getApiConfig } from '@/lib/api/util/config-helpers'
 
 export async function getServerSideProps(context: any) {
-  const details = await saveSellerToken(context.req, context.res)
+  let details
+  //  details = await saveSellerToken(context.req, context.res)
 
   const { query } = parse(context.req.url as string, true)
   const { redirect, isSeller } = query
@@ -17,6 +19,19 @@ export async function getServerSideProps(context: any) {
   console.log('currentPath: ', currentPath)
   console.log('destination: ', destination)
 
+  const clientId = getApiConfig().clientId
+  const sharedSecret = getApiConfig().sharedSecret
+  const authHost = getApiConfig().authHost
+  const apiHost = getApiConfig().apiHost
+  const adminUserHost = getApiConfig().adminUserHost
+  const env = {
+    clientId,
+    sharedSecret,
+    authHost,
+    apiHost,
+    adminUserHost,
+  }
+
   return {
     // redirect: {
     //   destination: destination + (isSeller ? '&isSeller=' + isSeller : ''),
@@ -25,17 +40,19 @@ export async function getServerSideProps(context: any) {
     props: {
       currentPath,
       destination,
-      details,
+      details: details || {},
+      env,
     },
   }
 }
 
-const AdminRedirect = (props: { currentPath: any; url: string; details: any }) => {
+const AdminRedirect = (props: { currentPath: any; url: string; details: any; env: any }) => {
   return (
     <div>
       <h2>currentPath: {props.currentPath}</h2>
       <h2>url: {props.url}</h2>
       <h2>details:: {JSON.stringify(props.details)}</h2>
+      <h2>env:: {JSON.stringify(props.env)}</h2>
     </div>
   )
 }
