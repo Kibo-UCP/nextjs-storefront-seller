74b5a2a43b3e6b06011320165be7c19674e6998f
Kibo-TriptiV <122886510+Kibo-TriptiV@users.noreply.github.com>
2024-01-16 18:18:53 +0530

    icky-976 added loadingbutton on save shipping addr

--- components/page-templates/B2B/QuoteDetailsTemplate/QuoteDetailsTemplate.tsx
+++ components/page-templates/B2B/QuoteDetailsTemplate/QuoteDetailsTemplate.tsx
@@ -5,6 +5,7 @@ import { FiberManualRecord } from '@mui/icons-material'
 import ArrowBackIos from '@mui/icons-material/ArrowBackIos'
 import Close from '@mui/icons-material/Close'
 import Done from '@mui/icons-material/Done'
+import { LoadingButton } from '@mui/lab'
 import {
   Stack,
   Typography,
@@ -388,7 +389,10 @@ const QuoteDetailsTemplate = (props: QuoteDetailsTemplateProps) => {
               name: quote?.name as string,
               expirationDate: quote?.expirationDate,
             })
-            onAccountTitleClick()
+            console.log(updateQuote.isSuccess)
+            if (updateQuote.isSuccess) {
+              onAccountTitleClick()
+            }
           },
           title: isApproving ? t('approve-quote-title') : t('submit-quote-title'),
           contentText: isApproving
@@ -1140,15 +1144,16 @@ const QuoteDetailsTemplate = (props: QuoteDetailsTemplateProps) => {
                               >
                                 {t('cancel')}
                               </Button>
-                              <Button
+                              <LoadingButton
                                 variant="contained"
                                 color="inherit"
                                 style={{ textTransform: 'none' }}
                                 onClick={handleAddressValidationAndSave}
+                                loading={validateForm}
                                 {...(!isAddressFormValid && { disabled: true })}
                               >
                                 {t('save-shipping-address')}
-                              </Button>
+                              </LoadingButton>
                             </Grid>
                           </Grid>
                         </Box>
