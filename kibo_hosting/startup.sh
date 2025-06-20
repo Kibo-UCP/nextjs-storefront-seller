8bda30f08e235c611315462b70482ec836538ab5
kibo-kevinwatts <85258296+kibo-kevinwatts@users.noreply.github.com>
2025-06-20 10:55:55 -0500

    fix: switch the hp url buidling logic

--- kibo_hosting/startup.sh
+++ kibo_hosting/startup.sh
@@ -1,2 +1,4 @@
 dotnet /app/tools/net6.0/any/Mozu.Core.ConfigXformer.dll -d . -e docker_template
+sed -i 's/^KIBO_APPDEV_HOST=http:\/\//KIBO_APPDEV_HOST=/' .env
+sed -i 's/^KIBO_ADMIN_USER_HOST=http:\/\//KIBO_ADMIN_USER_HOST=/' .env
 npm run start
